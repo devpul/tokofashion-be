f1021ea21f22b8e6ddff9aedb60da1f5140a912a
ibnubachdar <ibnubachdar@gmail.com>
2026-02-27 14:46:19 +0700

    Initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function validateProduct()
+    {
+
+    }
+
+    public function storeProduct()
+    {
+
+    }
+
+    public function updateProduct()
+    {
+
+    }
+
+    public function deleteProduct()
+    {
+        
+    }
+}
